07edcb22b0427d58bff0c598806af6b18cdd4100
enovid <emahn17@gmail.com>
2021-01-05 19:57:45 -0800

    Update vish.py

--- vish.py
+++ vish.py
@@ -6,6 +6,9 @@ import cProfile
 import pstats
 from pstats import SortKey
 import time
+from alive_progress import alive_bar
+from multiprocessing import Pool
+from functools import partial
 
 def timestamp_ms2str(ms):
     hours = int(ms // 3.6e+6)  
@@ -16,6 +19,8 @@ def timestamp_ms2str(ms):
 
 def resize_frame_cpu(frame, w, h):
     return cv.resize(frame, (w, h), interpolation=cv.INTER_AREA)
+
+
 
 frame_device = cv.cuda_GpuMat()
 def resize_frame_gpu(frame, w, h):
@@ -55,78 +60,81 @@ def create_video_sheet(path, gridsize=(3, 3), outputsize=(1920, 1080), preview=F
         codec = cv.VideoWriter_fourcc('D', 'I', 'V', 'X')
         output_video = cv.VideoWriter(f'{savedir}/{Path(path).stem}_vish{NROWS}x{NCOLS}.mp4', codec, fps, (PARENT_WIDTH, PARENT_HEIGHT))
 
+    def proc_next_frame(stream):
+        retval, frame = s.read()
+        if not retval:
+            return
+
+        # Resize the frame to fit in a grid of given dimension, specified by `outputsize`
+        frame = resize_frame_cpu(frame=frame, w=TILE_WIDTH, h=TILE_HEIGHT)
+
+        # Add time stamp to frame
+        current_ms = s.get(cv.CAP_PROP_POS_MSEC)
+        timestamp = timestamp_ms2str(ms=current_ms)
+        cv.putText(frame, timestamp, (20, TILE_HEIGHT - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        frames.append(frame)
+        return frame
+
+    frames = []
+    pool = Pool()
     scroll_offset = 0 
-    while scroll_offset < TILE_WIDTH: # The first tile starts off left of the screen
-        print(f'scroll_offset: {scroll_offset:.2f} px')
-        print(f'TILE_WIDTH: {TILE_WIDTH}')
-        print(f'{int(scroll_offset/TILE_WIDTH*100)}%')
-        # Read in the frames
-        frame_count = 0
-        resize_start_time = time.time()
-        frames = []
-        for s in streams: 
-            retval, frame = s.read()
-            if not retval:
-                continue
-            frame_count += 1
+    with alive_bar(manual=True) as bar:
+        while scroll_offset < TILE_WIDTH: # The first tile starts off left of the screen
+            # Progress bar
+            completion_pct = '%.2f'%(scroll_offset/TILE_WIDTH)
+            bar(completion_pct)
 
-            # Resize the frame to fit in a grid of given dimension, specified by `outputsize`
-            # frame = resize_frame_cpu(frame=frame, w=TILE_WIDTH, h=TILE_HEIGHT)
-            frame = resize_frame_gpu(frame=frame, w=TILE_WIDTH, h=TILE_HEIGHT)
+            # Read in the frames
+            frames = []
+            pool.map(proc_next_frame, streams)
+            # for s in streams: 
+                # frame = proc_next_frame(s)
+                # frames.append(frame)
 
-            # Add time stamp to frame
-            current_ms = s.get(cv.CAP_PROP_POS_MSEC)
-            timestamp = timestamp_ms2str(ms=current_ms)
-            cv.putText(frame, timestamp, (20, TILE_HEIGHT - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-            frames.append(frame)
-        resize_end_time = time.time()
-        resize_time = (resize_end_time - resize_start_time)*1000/frame_count
-        print(f'Resize: {frame_count} frames, {resize_time:.2f} ms/frame')
+            # Concatenate the frames required for a single frame of the output video into one long frame
+            full_frame = np.hstack(frames)
 
-        # Concatenate the frames required for a single frame of the output video into one long frame
-        full_frame = np.hstack(frames)
+            # Construct grid rows using sliding windows
+            rows = []
+            row = None
+            for i in range(1, NSTREAMS+1): 
+                start = int((TILE_WIDTH * i) - scroll_offset)
+                end = int(start + TILE_WIDTH)
+                window = full_frame[:, start:end]
 
-        # Construct grid rows using sliding windows
-        rows = []
-        row = None
-        for i in range(1, NSTREAMS+1): 
-            start = int((TILE_WIDTH * i) - scroll_offset)
-            end = int(start + TILE_WIDTH)
-            window = full_frame[:, start:end]
+                # Append the current window to our row
+                if row is None:
+                    row = window
+                else:
+                    row = np.hstack((row, window))
 
-            # Append the current window to our row
-            if row is None:
-                row = window
-            else:
-                row = np.hstack((row, window))
+                # Wrap the frame to a new row
+                row_width = row.shape[1]
+                if row_width > PARENT_WIDTH:
+                    rows.append(row[:,:PARENT_WIDTH])
+                    row = row[:,PARENT_WIDTH:]
 
-            # Wrap the frame to a new row
-            row_width = row.shape[1]
-            if row_width > PARENT_WIDTH:
-                rows.append(row[:,:PARENT_WIDTH])
-                row = row[:,PARENT_WIDTH:]
+            # Construct final frame
+            final_frame = np.vstack(rows)
+            if save: 
+                output_video.write(final_frame)
 
-        # Construct final frame
-        final_frame = np.vstack(rows)
-        if save: 
-            output_video.write(final_frame)
+            # Show live preview
+            if preview: 
+                cv.imshow('frame', final_frame)
+                if cv.waitKey(ms_per_frame) & 0xFF == ord('q'):
+                    cv.destroyAllWindows()
 
-        # Show live preview
-        if preview: 
-            cv.imshow('frame', final_frame)
-            if cv.waitKey(ms_per_frame) & 0xFF == ord('q'):
-                cv.destroyAllWindows()
-
-        scroll_offset += px_scroll_per_frame
+            scroll_offset += px_scroll_per_frame
 
     for s in streams:
         s.release()
 
-GRID_WIDTH  = 3
-GRID_HEIGHT = 3
+GRID_WIDTH  = 10
+GRID_HEIGHT = 10
 PREVIEW = True
-SAVE_DIR = 'C:/Users/enovid/github/scrolling-video-sheets/output'
-VIDEO_PATH = 'C:\Users\enovid\github\scrolling-video-sheets\sample_data\sample.avi'
+SAVE_DIR = '/home/enovid/repos/scrolling-video-sheets/output'
+VIDEO_PATH = '/home/enovid/repos/scrolling-video-sheets/sample_data/BigBuckBunny1080p.mp4'
 
 
 if __name__ == '__main__':
